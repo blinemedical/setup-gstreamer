a8dff746a77f4b1eabd5cbe87e17f038ccdad704
Jeffery Wilson <jeffery.wilson@laerdal.com>
2022-06-13 14:48:51 -0400

    log test output

--- index.test.js
+++ index.test.js
@@ -7,6 +7,11 @@ test('test runs', () => {
   process.env['INPUT_VERSION'] = '1.19.90';
   process.env['INPUT_ARCH'] = 'x86_64';
   const ip = path.join(__dirname, 'index.js');
-  let result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
-  console.log(result);
+  try {
+    let result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+    console.log(result);
+  } catch (err) {
+    console.error(err.stdout.toString());
+    throw err;
+  }
 });
