196b24c5074c54ce56b2daef7c0eec8f90e38617
Lyra McMillan <lyra.mcmillan@laerdal.com>
2024-01-23 11:48:26 -0500

    [VIDEO-2683] - Fixed and added environment variables in support of functional testing

--- index.test.js
+++ index.test.js
@@ -6,7 +6,8 @@ const path = require('path');
 test('test runs', () => {
   process.env['INPUT_VERSION'] = '1.22.7';
   process.env['INPUT_ARCH'] = 'x86_64';
-  process.env['INPUT_GSTREAMEROPTIONS'] = '--disable-gtk-doc --disable-examples --disable-tests --disable-benchmarks --disable-debug --disable-debugutils --disable-gtk-doc-html --disable-gtk-doc-pdf --disable-introspection --disable-valgrind --disable-examples --disable-tests --disable-benchmarks --disable-debug --disable-debugutils --disable-gtk-doc-html --disable-gtk-doc-pdf --disable-introspection --disable-valgrind --disable-examples --disable-tests --disable-benchmarks --disable-debug --disable-debugutils --disable-gtk-doc-html --disable-gtk-doc-pdf --disable-introspection --disable-valgrind --disable-examples --disable-tests --disable-benchmarks --disable-debug --disable-debugutils --disable-gtk-doc-html --disable-gtk-doc-pdf --disable-introspection --disable-valgrind --disable-examples --disable-tests --disable-benchmarks --disable-debug --disable-debugutils --disable-gtk-doc-html --disable-gtk-doc-pdf --disable-introspection --disable-valgrind';
+  process.env['INPUT_REPOURL'] = 'https://gitlab.freedesktop.org/gstreamer/gstreamer.git';
+  process.env['INPUT_GSTREAMEROPTIONS'] = '-Dlibnice:tests=disabled,-Dlibnice:examples=disabled,-Dopenh264:tests=disabled,-Dpygobject:tests=false,-Dpython=enabled,-Dlibav=enabled,-Dugly=enabled,-Dbad=enabled,-Ddevtools=enabled,-Dges=enabled,-Drtsp_server=enabled,-Dvaapi=enabled,-Dsharp=disabled,-Dgpl=enabled,-Dpython=disabled,-Dvaapi=disabled,-Dgst-plugins-base:pango=enabled,-Dgst-plugins-good:cairo=enabled,-Dgst-plugins-good:soup=enabled,-Dgst-plugins-good:soup-lookup-dep=true';
   const ip = path.join(__dirname, 'index.js');
   if (process.platform === 'linux') {
     // Linux uses caching, so until there's a way to mock around @actions/cache,
